bd8a691f49d1f77760a07c8e74ded3d0cf94a839
Dana White <dana.white@stackhawk.com>
2022-08-19 14:36:43 -0600

    Added in okta-resource-owner-password auth script

---
+++ scripts/examples/authentication/okta-resource-owner-password.js
@@ -0,0 +1,61 @@
+var HttpRequestHeader = Java.type('org.parosproxy.paros.network.HttpRequestHeader');
+var HttpHeader = Java.type("org.parosproxy.paros.network.HttpHeader");
+var URI = Java.type('org.apache.commons.httpclient.URI');
+var AuthenticationHelper = Java.type('org.zaproxy.zap.authentication.AuthenticationHelper');
+var Base64 = Java.type("java.util.Base64");
+
+function authenticate(helper, paramsValues, credentials) {
+    print("Okta grant_type=password...");
+
+    // set login path and http
+    var requestUri = new URI(paramsValues.get("issuer"), false);
+    var requestMethod = HttpRequestHeader.POST;
+    var requestHeader = new HttpRequestHeader(requestMethod, requestUri, HttpHeader.HTTP11);
+
+    //build static request headers
+    requestHeader.setHeader("Content-Type", "application/x-www-form-urlencoded");
+    requestHeader.setHeader("Accept", "application/json");
+    requestHeader.setHeader("Cache-control", "no-cache");
+
+    // encode client id / secret
+    var basicAuthString = base64Encode((credentials.getParam('client_id') + ':' + credentials.getParam('client_secret')));
+
+    // set the Basic Auth Header
+    requestHeader.setHeader("Authorization", "Basic " + basicAuthString);
+
+    //build request body
+    var requestBody = "grant_type=" + paramsValues.get('grant_type') +
+        "&username=" + credentials.getParam('username') +
+        "&password=" + credentials.getParam('password') +
+        "&scope=" + paramsValues.get('scope');
+
+    // build final post
+    var msg = helper.prepareMessage();
+    msg.setRequestHeader(requestHeader);
+    msg.setRequestBody(requestBody);
+    print("MSG request header: " + msg.requestHeader)
+    print("MSG request body: " + msg.requestBody)
+    requestHeader.contentLength = msg.requestBody.length();
+
+    //send message
+    helper.sendAndReceive(msg);
+
+    return msg;
+}
+
+function base64Encode(originalInput) {
+    return Base64.getEncoder().encodeToString(originalInput.getBytes());
+}
+
+function getRequiredParamsNames(){
+    return ["issuer", "grant_type", "scope"];
+}
+
+function getOptionalParamsNames(){
+    return [""];
+}
+
+function getCredentialsParamsNames(){
+    return ["client_id", "client_secret", "username", "password"];
+}
+
