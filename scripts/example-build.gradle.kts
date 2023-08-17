3b1657bd3d46d9d058f23c5b1f6fcb9476490ee4
KC Berg <kc.berg@stackhawk.com>
2023-08-17 14:32:11 -0600

    an example build.gradle.kts file for enabling IDE and dependency support for hawkscripting kotlin files

---
+++ scripts/example-build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.konan.file.File.Companion.userHome
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+}
+
+val kotlinVersion = "1.7.20"
+val hawkScriptSdkVersion = "3.1.12"
+
+kotlin {
+    sourceSets {
+        main {
+            kotlin {
+                srcDirs(
+                    "authentication",
+                    "session",
+                    "httpsender",
+                    "active",
+                    "proxy",
+                )
+            }
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    compileOnly("org.jetbrains.kotlin:kotlin-script-runtime:$kotlinVersion")
+    // change to location of the hawkscript-sdk-<version>.zip file.
+    compileOnly(zipTree("$userHome/Downloads/hawkscript-sdk-$hawkScriptSdkVersion.zip"))
+}
