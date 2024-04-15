917893e7fab65912e9879291d9003f397798530c
Dana White <dana.white@stackhawk.com>
2024-04-15 13:35:06 -0600

    Made token name configurable

--- scripts/examples/session/firebase-id-token-session.kts
+++ scripts/examples/session/firebase-id-token-session.kts
@@ -1,12 +1,9 @@
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.node.ObjectNode
+import com.stackhawk.zap.extension.talon.session.HawkBasedSessionHelper
 import org.apache.commons.httpclient.Cookie
 import org.apache.log4j.LogManager
 import org.zaproxy.zap.session.ScriptBasedSessionManagementMethodType
 
 val logger = LogManager.getLogger("firebase-id-token")
-val mapper = ObjectMapper()
-
 // This function is called after the authentication function to establish a session.
 // The sessionWrapper.httpMessage will contain the responseBody, responseHeader and requestingUser which can be used to
 // gather data pertaining to the authentication status such as cookies, tokens or data from the responseBody
@@ -14,12 +11,14 @@ fun extractWebSession(sessionWrapper: ScriptBasedSessionManagementMethodType.Ses
     val tokenName = sessionWrapper.getParam("tokenName")
     val cookieName = sessionWrapper.getParam("cookieName")
     val domain = sessionWrapper.getParam("domain")
-    val jsonObject = mapper.readValue(sessionWrapper.httpMessage.responseBody.bytes, ObjectNode::class.java)
-    val cookieValue = jsonObject.get(tokenName).asText()
-    val cookie = Cookie(domain, cookieName, cookieValue)
+    val cookieValue =  HawkBasedSessionHelper.getTokenFromJson(sessionWrapper.httpMessage.responseBody, tokenName)
+    cookieValue?.let {
+        val cookie = Cookie(domain, cookieName, it)
+        logger.debug("Adding session cookie ${cookie.name} wtih value ${cookie.value}" )
+        sessionWrapper.session.httpState.addCookie(cookie)
+    }
 
-    logger.debug("Adding session cookie ${cookie.name} wtih value ${cookie.value}" )
-    sessionWrapper.session.httpState.addCookie(cookie)
+
 }
 
 // This function is called on each request allow the reuqest to be modified before it is sent to the web application.
@@ -33,7 +32,7 @@ fun clearWebSessionIdentifiers(sessionWrapper: ScriptBasedSessionManagementMetho
 
 // The required parameter names for your script, your script will throw an error if these are not supplied in the sessionScript.parameters configuration.
 fun getRequiredParamsNames(): Array<String> {
-    return arrayOf("cookieName", "domain")
+    return arrayOf("cookieName", "domain", "tokenName")
 }
 
 fun getOptionalParamsNames(): Array<String> {
