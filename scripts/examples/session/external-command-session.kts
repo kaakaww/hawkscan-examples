c77b7ceb21c0168c5f1a33bcdab2e9886497fa15
April Conger <april.conger@stackhawk.com>
2024-08-27 10:38:41 -0600

    added some comments

--- scripts/examples/session/external-command-session.kts
+++ scripts/examples/session/external-command-session.kts
@@ -3,24 +3,33 @@ import org.apache.log4j.Logger
 import com.stackhawk.hste.session.ScriptBasedSessionManagementMethodType
 import com.stackhawk.hste.extension.script.ScriptVars
 
+/* external-command-session.kts
+ *  This HawkScan session script runs an external command for every request, and adds any cookies or headers returned
+ *  from the external command to the current scanner request.
+ *
+ *  Required params:
+ *      sessionCommand - the relative path to the external command to run
+ *  Optional params:
+ *      inputData - a string that will be passed to the external command environment as the env var, INPUT_DATA
+ *
+ *  External Command Inputs (via environment variables):
+ *      INPUT_DATA - The data from the inputData param
+ *      MSG_HEADERS - The headers from the current HTTP message
+ *      MSG_BODY - The body from the current HTTP message
+ *  External Command Outputs:
+ *      The external command is expected to output a JSON blob to std-out containing any headers or cookies to add to
+ *      the current request. See https://tinyurl.com/yjeeswwf for an example.
+ */
+
 val logger: Logger = LogManager.getLogger("external-command-session")
 
 // This function is called after the authentication function to establish a session.
 // The sessionWrapper.httpMessage will contain the responseBody, responseHeader and requestingUser which can be used to
 // gather data pertaining to the authentication status such as cookies, tokens or data from the responseBody
 fun extractWebSession(sessionWrapper: ScriptBasedSessionManagementMethodType.SessionWrapper) {
-    var cookieIterator = 0
-    logger.info("Setting session cookies")
-    while (ScriptVars.getGlobalVars().containsKey("cookieName$cookieIterator")) {
-        // Change domain to match the domain of your site
-        val domain = "localhost"
-        val httpCookie = org.apache.commons.httpclient.Cookie(domain,
-            ScriptVars.getGlobalVar("cookieName$cookieIterator"),
-            ScriptVars.getGlobalVar("cookieValue$cookieIterator"))
-        logger.info("Adding session cookie ${httpCookie.name} wtih value ${httpCookie.value}" )
-        sessionWrapper.session.httpState.addCookie(httpCookie)
-        cookieIterator++
-    }
+    /* We assume that authentication has been already been handled. In this script, our only concern is to further
+     * modify requests during scanning. So there's nothing to do here.
+     */
 }
 
 // This function is called on each request allow the request to be modified before it is sent to the web application.
